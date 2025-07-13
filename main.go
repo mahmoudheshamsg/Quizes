5dcd3b13fd25cbba012ba128cba1dee9586860aa
mahmoudheshamsg <mahmoudheshamsg7@gmail.com>
2025-07-13 05:18:09 +0300

    added a time limit to the quiz

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
+	"time"
 )
 
 func main() {
@@ -28,23 +28,29 @@ func main() {
 		fmt.Println("Error reading CSV:", err)
 		return
 	}
-
+	timer := time.NewTimer(time.Duration(*limit) * time.Second)
 	correct := 0
 	l := len(record)
 	// Iterate over all the CSV enteries
 	for i := 0; i < l; i++ {
-		fmt.Println(record[i][0])
-		rans, e := strconv.Atoi(record[i][1])
-		if e != nil {
+		select {
+		// Shutdown the program when the timer fires off
+		case <-timer.C:
+			fmt.Print("Time have run out\n")
+			fmt.Printf("You have scored %d out of %d\n", correct, l)
+			return
+		default:
+			fmt.Println(record[i][0])
+			var ans string
+			fmt.Scan(&ans)
+			// Increment the counter for each correct answer
+			if ans == record[i][1] {
+				correct++
+			}
 
 		}
-		var ans int
-		fmt.Scan(&ans)
-		// Increment the counter for each correct answer
-		if ans == rans {
-			correct++
-		}
+
 	}
-	fmt.Println(correct)
+	fmt.Printf("You have scored %d out of %d\n", correct, l)
 
 }
